e292b5e2f8f1899c42809be1c951dd95993e73e1
DeniseXavier06 <denisexavier062019@gmail.com>
2022-01-12 13:42:48 -0300

    ccriando operacao de instanciacao

--- src/main/java/com/xavier/web/WebApplication.java
+++ src/main/java/com/xavier/web/WebApplication.java
@@ -1,13 +1,38 @@
 package com.xavier.web;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class WebApplication {
+import com.xavier.web.domain.Playlist;
+import com.xavier.web.repositeries.PlaylistRepositoy;
 
+@SpringBootApplication
+public class WebApplication implements CommandLineRunner {
+
+	@Autowired
+	private PlaylistRepositoy playlistRepositoy;
+	
 	public static void main(String[] args) {
 		SpringApplication.run(WebApplication.class, args);
 	}
+
+	
+	//permite executar algumas acoes
+	@Override
+	public void run(String... args) throws Exception {
+		Playlist play1 = new Playlist(null, "Denise - 2022", "Curados para curar!");
+		Playlist play2 = new Playlist(null, "Cleopse -2022", "Rock!");
+		
+		//quem acessa os dados é o repository
+		playlistRepositoy.saveAll(Arrays.asList(play1, play2));
+		
+	}
+	
+	
+	
 
 }
